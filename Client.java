66b87b6a685ab4cfbf2c0c58187cb7e92ed9b299
Mohab Elshabasy <mohabelshabasy@gmail.com>
2025-11-10 23:45:59 -0500

    Skeleton Code

--- Client.java
+++ Client.java
@@ -1,3 +1,44 @@
+import java.net.*;
+
 public class Client {
-    
+
+    private String host;
+    private int port;
+    private Socket socket;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+        try {
+            this.socket = new Socket(host, port);
+        } catch (Exception e) {
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public void handshake() {
+
+    }
+
+    public String request(String str) {
+        return null;
+    }
+
+    public void disconnect() {
+        try {
+            socket.close();
+        } catch (Exception e) {
+        }
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,28 @@
+import java.time.*;
+import java.util.ArrayList;
+
+
 public class Server {
-    
+
+    private int port;
+
+    public Server (int port) {
+        this.port = port;
+    }
+
+    public void serve(int num) {
+
+    }
+
+
+    public void disconnect() {
+
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes() {
+        return null;
+    }
+
+     public static void main(String args[]){
+    }
 }
